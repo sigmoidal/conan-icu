15cfb54e7eedcb760865f016b46bd580abeadf3c
sigmoidal <unknown>
2017-12-30 14:24:20 +0200

    Fixing CI templates

--- build.py
+++ build.py
@@ -1,8 +1,11 @@
-from conan.packager import ConanMultiPackager
-import os, re, platform
-from distutils.util import strtobool
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from collections import defaultdict
+from conan.packager import ConanMultiPackager, split_colon_env
+import os
+import re
+import platform
+
 
 def get_value_from_recipe(search_string):
     with open("conanfile.py", "r") as conanfile:
@@ -10,11 +13,14 @@ def get_value_from_recipe(search_string):
         result = re.search(search_string, contents)
     return result
 
+
 def get_name_from_recipe():
-    return get_value_from_recipe(r'name\s*=\s*"(\S*)"').groups()[0]
+    return get_value_from_recipe(r'''name\s*=\s*["'](\S*)["']''').groups()[0]
+
 
 def get_version_from_recipe():
-    return get_value_from_recipe(r'version\s*=\s*"(\S*)"').groups()[0]
+    return get_value_from_recipe(r'''version\s*=\s*["'](\S*)["']''').groups()[0]
+
 
 def get_default_vars():
     username = os.getenv("CONAN_USERNAME", "bincrafters")
@@ -22,67 +28,66 @@ def get_default_vars():
     version = get_version_from_recipe()
     return username, channel, version
 
+
 def is_ci_running():
-    return os.getenv("APPVEYOR_REPO_NAME","") or os.getenv("TRAVIS_REPO_SLUG","")
+    return os.getenv("APPVEYOR_REPO_NAME", "") or os.getenv("TRAVIS_REPO_SLUG", "")
+
 
 def get_ci_vars():
-    reponame_a = os.getenv("APPVEYOR_REPO_NAME","")
-    repobranch_a = os.getenv("APPVEYOR_REPO_BRANCH","")
+    reponame_a = os.getenv("APPVEYOR_REPO_NAME", "")
+    repobranch_a = os.getenv("APPVEYOR_REPO_BRANCH", "")
 
-    reponame_t = os.getenv("TRAVIS_REPO_SLUG","")
-    repobranch_t = os.getenv("TRAVIS_BRANCH","")
+    reponame_t = os.getenv("TRAVIS_REPO_SLUG", "")
+    repobranch_t = os.getenv("TRAVIS_BRANCH", "")
 
     username, _ = reponame_a.split("/") if reponame_a else reponame_t.split("/")
     channel, version = repobranch_a.split("/") if repobranch_a else repobranch_t.split("/")
     return username, channel, version
 
+
 def get_env_vars():
     return get_ci_vars() if is_ci_running() else get_default_vars()
+
 
 def get_os():
     return platform.system().replace("Darwin", "Macos")
 
-if __name__ == "__main__":
-    conan_build_options = {}
-    conan_build_settings = {}
 
+def get_build_types():
+    # Only Appveyor has CONFIGURATION, Travis uses both types anyway.
+    build_types_a = split_colon_env("CONFIGURATION") or ['Release', 'Debug']
+    build_types = split_colon_env("CONAN_BUILD_TYPES") or build_types_a
+    return build_types
+
+
+def get_remotes():
+    user_remote = "https://api.bintray.com/conan/{0}/public-conan".format(username)
+    bincrafters_remote = 'https://api.bintray.com/conan/bincrafters/public-conan'
+    remotes = [user_remote, bincrafters_remote]
+
+    # If the user supplied a remote manually we give him priority
+    # e.g. maybe he is trying to override user_remote or the bincrafters_remote repo.
+    remote_env = split_colon_env("CONAN_REMOTES")
+    if remote_env:
+        remotes = remote_env + remotes
+    return remotes
+
+
+if __name__ == "__main__":
     name = get_name_from_recipe()
     username, channel, version = get_env_vars()
     reference = "{0}/{1}".format(name, version)
     upload = "https://api.bintray.com/conan/{0}/public-conan".format(username)
 
-    os.environ["CONAN_UPLOAD"] = "https://api.bintray.com/conan/{0}/public-conan".format(username)
-    os.environ["CONAN_REMOTES"] = "https://api.bintray.com/conan/conan-community/conan, " \
-                                  "https://api.bintray.com/conan/bincrafters/public-conan"
+    builder = ConanMultiPackager(
+        username=username,
+        channel=channel,
+        reference=reference,
+        build_types=get_build_types(),
+        upload=upload,
+        remotes=get_remotes(),
+        upload_only_when_stable=True,
+        stable_branch_pattern="stable/*")
 
-    reference = "{0}/{1}".format(name, version)
-
-    builder = ConanMultiPackager(args="--build missing",
-                                 username=username,
-                                 channel=channel,
-                                 reference=reference,
-                                 upload=os.environ["CONAN_UPLOAD"],
-                                 remotes=os.environ["CONAN_REMOTES"],
-                                 upload_only_when_stable=True,
-                                 stable_branch_pattern="stable/*")
-
-    if "CONAN_ICU_SHARED" in os.environ:
-        conan_build_options.update({"icu:shared": True if strtobool(os.environ["CONAN_ICU_SHARED"]) else False})
-
-    if "CONAN_ICU_DATA_PACKAGING" in os.environ:
-        conan_build_options.update({"icu:data_packaging": os.environ["CONAN_ICU_DATA_PACKAGING"]})
-
-
-    if 'TRAVIS_OS_NAME' not in os.environ:
-        if "CONAN_ICU_MSVC_PLATFORM" in os.environ:
-            conan_build_options.update({"icu:msvc_platform": os.environ["CONAN_ICU_MSVC_PLATFORM"]})
-    else:
-        if os.environ['TRAVIS_OS_NAME'] == 'linux':
-            conan_build_settings.update({"icu:os": "Linux"})
-        if os.environ['TRAVIS_OS_NAME'] == 'osx':
-            conan_build_settings.update({"icu:os": "Macos"})
-
-    builder.add(settings=conan_build_settings, options=conan_build_options)
-
+    builder.add_common_builds(shared_option_name=name + ":shared")
     builder.run()
-
